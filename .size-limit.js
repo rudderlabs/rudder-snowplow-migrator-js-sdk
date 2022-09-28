556b931ffe872f128adf7b929b547b1d27c4c78e
Moumita <mandal.moumita2013@gmail.com>
2022-09-28 18:14:41 +0530

    Initial folder structure

---
+++ .size-limit.js
@@ -0,0 +1,12 @@
+/**
+ * DO NOT EDIT the size limits configured in this file.
+ * Should you update them, please contact CODEOWNERS.
+ */
+module.exports = [
+  {
+    name: 'rudder-snowplow-adapter',
+    path: 'dist/rudder-sp.min.js',
+    gzip: true,
+    limit: '40 kB',
+  }
+];
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import { getOutputConfiguration, getOutputFilePath, getDefaultConfig } from './rollupUtils';
+
+const outDir = 'dist';
+const distName = 'rudder-sp-adapter';
+
+const outFilePath = getOutputFilePath(outDir, distName);
+
+const outputFiles = getOutputConfiguration(
+  `${outDir}/rudder-snowplow-adapter`,
+  'rudderstackSpAdapter',
+  outFilePath,
+);
+
+export default {
+  ...getDefaultConfig(distName),
+  input: 'src/index.js',
+  output: outputFiles,
+};
