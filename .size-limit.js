aaf145462d6b49982854f7ed0869ef251d437821
George Bardis <gbardis@rudderstack.com>
2022-10-13 11:51:57 +0300

    ci: change bundlesize to size-limit

---
+++ .size-limit.js
@@ -0,0 +1,12 @@
+/**
+ * DO NOT EDIT the size limits configured in this file.
+ * Should you update them, please contact CODEOWNERS.
+ */
+module.exports = [
+  {
+    name: 'Snowplow Adapter - CDN',
+    path: 'dist/rudder-snowplow-adapter.min.js',
+    gzip: true,
+    limit: '4 kB',
+  },
+];
